36e17c6aa8bbc0d47b6b701301e4dda2d6474ed4
sirekanyan <sirekanyan@gmail.com>
2017-09-17 19:45:59 +0300

    Added get credentials api method

--- app/src/main/java/me/vadik/knigopis/Endpoint.kt
+++ app/src/main/java/me/vadik/knigopis/Endpoint.kt
@@ -1,11 +1,19 @@
 package me.vadik.knigopis
 
 import me.vadik.knigopis.model.Book
+import me.vadik.knigopis.model.Credentials
 import me.vadik.knigopis.model.User
 import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface Endpoint {
+
+  @FormUrlEncoded
+  @POST("/user/get-credentials")
+  fun getCredentials(@Field("token") token: String): Call<Credentials>
 
   @GET("users/latest")
   fun latestUsers(): Call<Map<String, User>>
---
+++ app/src/main/java/me/vadik/knigopis/model/Credentials.kt
@@ -0,0 +1,23 @@
+package me.vadik.knigopis.model
+
+import com.google.gson.annotations.SerializedName
+
+class Credentials(
+    @SerializedName("access-token")
+    val accessToken: String,
+    val userFull: UserFull
+) {
+
+  class UserFull(
+      val id: String,
+      val lang: String,
+      val nickname: String,
+      val photo: String,
+      val profile: String,
+      val identity: String,
+      val booksCount: Int,
+      val subscriptions: Map<String, Int>,
+      val createdAt: String,
+      val updatedAt: String
+  )
+}
