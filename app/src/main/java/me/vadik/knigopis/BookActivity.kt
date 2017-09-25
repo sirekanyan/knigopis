6ded33f26c070ebd0b4da0d3175ed554a4a3829d
sirekanyan <sirekanyan@gmail.com>
2017-09-26 01:01:10 +0300

    Added action buttons; added hiding read date

--- app/src/main/java/me/vadik/knigopis/BookActivity.kt
+++ app/src/main/java/me/vadik/knigopis/BookActivity.kt
@@ -2,11 +2,44 @@ package me.vadik.knigopis
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.View.*
+import android.widget.CheckBox
 
 class BookActivity : AppCompatActivity() {
+
+  private val toolbar by lazy { findView<Toolbar>(R.id.toolbar) }
+  private val readCheckbox by lazy { findView<CheckBox>(R.id.book_read_checkbox) }
+  private val dateInputViews by lazy {
+    arrayOf<View>(
+        findView(R.id.book_year_input),
+        findView(R.id.book_month_input),
+        findView(R.id.book_day_input)
+    )
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.book_edit)
+    toolbar.inflateMenu(R.menu.book_menu)
+    toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+    toolbar.setNavigationOnClickListener {
+      finish()
+    }
+    toolbar.setOnMenuItemClickListener {
+      when (it.itemId) {
+        R.id.option_save_book -> {
+          finish()
+          true
+        }
+        else -> false
+      }
+    }
+    readCheckbox.setOnCheckedChangeListener { _, checked ->
+      dateInputViews.forEach {
+        it.visibility = if (checked) VISIBLE else GONE
+      }
+    }
   }
 }
