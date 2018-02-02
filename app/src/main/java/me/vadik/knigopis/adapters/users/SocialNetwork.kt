b001e1ff9d5ed773a59a326e42344fb70a3c98f0
sirekanyan <sirekanyan@gmail.com>
2018-02-02 23:08:15 +0300

    Added helper class for social networks

---
+++ app/src/main/java/me/vadik/knigopis/adapters/users/SocialNetwork.kt
@@ -0,0 +1,15 @@
+package me.vadik.knigopis.adapters.users
+
+import android.net.Uri
+import android.support.annotation.StringRes
+import me.vadik.knigopis.R
+
+fun Uri.toSocialNetwork() = SocialNetwork.values().find { it.host == host }
+
+enum class SocialNetwork(@StringRes val titleRes: Int, val host: String) {
+    FACEBOOK(R.string.social_facebook, "www.facebook.com"),
+    TWITTER(R.string.social_twitter, "twitter.com"),
+    GOOGLE(R.string.social_google_plus, "plus.google.com"),
+    VK_COM(R.string.social_vk_com, "vk.com"),
+    MAIL_RU(R.string.social_mail_ru, "my.mail.ru");
+}
