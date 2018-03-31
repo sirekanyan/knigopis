166e86774350d3887b46652f4254b643222f0a8c
Vadik Sirekanyan <sirekanyan@gmail.com>
2018-03-31 10:30:11 +0300

    Added instagram social network

--- app/src/main/java/me/vadik/knigopis/adapters/users/SocialNetwork.kt
+++ app/src/main/java/me/vadik/knigopis/adapters/users/SocialNetwork.kt
@@ -8,6 +8,7 @@ fun Uri.toSocialNetwork() = SocialNetwork.values().find { it.host == host }
 
 enum class SocialNetwork(@StringRes val titleRes: Int, val host: String) {
     FACEBOOK(R.string.social_facebook, "www.facebook.com"),
+    INSTAGRAM(R.string.social_instagram, "www.instagram.com"),
     TWITTER(R.string.social_twitter, "twitter.com"),
     GOOGLE(R.string.social_google_plus, "plus.google.com"),
     VK_COM(R.string.social_vk_com, "vk.com"),
