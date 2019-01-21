0c20682395cec771a0b1568812c1e341e77e5bda
Vadik Sirekanyan <sirekanyan@gmail.com>
2019-01-21 20:07:43 +0300

    Changed http to https for api requests

--- app/src/main/java/com/sirekanyan/knigopis/Constants.kt
+++ app/src/main/java/com/sirekanyan/knigopis/Constants.kt
@@ -1,6 +1,6 @@
 package com.sirekanyan.knigopis
 
-const val MAIN_API = "http://api.knigopis.com"
+const val MAIN_API = "https://api.knigopis.com"
 const val STATIC_API = "https://knigopis.tk/img"
 const val DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"
 const val MIN_BOOK_PRIORITY = 1
