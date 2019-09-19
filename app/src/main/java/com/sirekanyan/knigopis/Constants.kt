4a0e4e3fec813897a600feebb2e220bf1d2603e2
Vadik Sirekanyan <sirekanyan@gmail.com>
2019-09-20 02:08:25 +0300

    Changed static api url

--- app/src/main/java/com/sirekanyan/knigopis/Constants.kt
+++ app/src/main/java/com/sirekanyan/knigopis/Constants.kt
@@ -1,7 +1,7 @@
 package com.sirekanyan.knigopis
 
 const val MAIN_API = "https://api.knigopis.com"
-const val STATIC_API = "https://knigopis.tk/img"
+const val STATIC_API = "https://static.sirekanyan.com/img"
 const val DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"
 const val MIN_BOOK_PRIORITY = 1
 const val MAX_BOOK_PRIORITY = 100
